c9b3ae257cdf0e50ee8c665c92c75b778eb2caef
Your Name <ayushgandhi904@gmail.com>
2023-07-24 10:44:46 +0530

    folder creation

---
+++ template.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level = logging.INFO, format = "[%(asctime)s]: %(message)s:")
+
+project_name = "chicken_classifier"
+
+list_of_files = [
+    ".githbub/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/checks.ipynb"    
+]
+
+for file_path in list_of_files:
+    filepath = Path(file_path)
+    filedir, filename = os.path.split(file_path)
+    
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+        
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass
+            logging.info(f"Creating empty file {file_path}")
+    else:
+        logging.info(f"{filename} already exists")
