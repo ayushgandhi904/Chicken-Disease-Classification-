88ead1c3702cc1744f2e9ccbb27666f2e1463feb
Your Name <ayushgandhi904@gmail.com>
2023-07-25 09:34:11 +0530

    model trained

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from chicken_classifier import logger
 from chicken_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chicken_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from chicken_classifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion"
 
@@ -13,12 +14,30 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
+
 STAGE_NAME = "Prepare base model"
 
 if __name__ == "__main__":
     try:
         logger.info(f"----------{STAGE_NAME} started ----------")
         obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f"----------{STAGE_NAME} completed----------")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+    
+    
+STAGE_NAME = "Training"
+
+if __name__ == "__main__":
+    try:
+        logger.info(f"----------{STAGE_NAME} started ----------")
+        obj = ModelTrainingPipeline()
         obj.main()
         logger.info(f"----------{STAGE_NAME} completed----------")
     except Exception as e:
